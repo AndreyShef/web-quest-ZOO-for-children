8043f99170bb6213cee69b79fb8714d7034ad2fc
AndreyShef <nava2007@yandex.ru>
2021-10-26 10:48:04 +0300

    открытие модальных окон 'кто такой...'

--- js/script.js
+++ js/script.js
@@ -11,40 +11,52 @@ document.addEventListener("DOMContentLoaded", () => {
                         </div>
                     </div> 
                 `;
-body.innerHTML = beginer;
-const btnBegin = document.querySelector('.btn'),
-      div = document.querySelector('.wrapper-beginer');
-div.style.transition = '1s';
-btnBegin.addEventListener('click', (e) => {
-    setTimeout(() =>{ 
-        div.style.opacity = 0; 
-        setTimeout(() => div.remove(), 600);
-    }, 800);
-    body.append(container);
+    body.innerHTML = beginer;
 
-
-    const author = document.querySelector('.wrapper-author-img'),
-        dialog = document.querySelector('.wrapper-author-dialog'),
-        wrapBtnChoice = document.querySelector('.choice'),
-        btnChoice = document.querySelector('.btn-choice'),
-        choice = document.querySelector('.wrapper-choice');
-
-    btnChoice.addEventListener('click', (e) => {
-        author.remove();
-        dialog.remove();
-        wrapBtnChoice.remove();
-        choice.style.display = 'block';
-
-    });
-    const zooModal = document.querySelector('#myModal'),
-          btnZooModal = document.querySelector('.btn-zoologist-who_is_it');
-
-    btnZooModal.addEventListener('click', () => {
-        zooModal.classList.remove('fade');
-        zooModal.classList.add('show');
-        
-    });
+    // После нажатия вперёд! (начало квеста)
+    const btnBegin = document.querySelector('.btn'),
+        div = document.querySelector('.wrapper-beginer');
+    div.style.transition = '1s';
     
-});
+    btnBegin.addEventListener('click', (e) => {
+        setTimeout(() =>{ 
+            div.style.opacity = 0; 
+            setTimeout(() => div.remove(), 600);
+        }, 800);
+        body.append(container);
+
+        // Удаление диалога автора
+        const author = document.querySelector('.wrapper-author-img'),
+            dialog = document.querySelector('.wrapper-author-dialog'),
+            wrapBtnChoice = document.querySelector('.choice'),
+            btnChoice = document.querySelector('.btn-choice'),
+            choice = document.querySelector('.wrapper-choice');
+
+        btnChoice.addEventListener('click', (e) => {
+            author.remove();
+            dialog.remove();
+            wrapBtnChoice.remove();
+            choice.style.display = 'block';
+
+        });
+
+        // Модальные окна - кто такие зоолог и следопыт
+        const zooModal = document.querySelector('#zooModal'),
+              pathfinderModal = document.querySelector('#pathfinderModal'),
+              btnZooModal = document.querySelector('.btn-zoologist-who_is_it'),
+              btnPathfinderModal = document.querySelector('.btn-pathfinder-who_is_it');
+
+        function openModal(button, modal) {
+            button.addEventListener('click', () => {
+                modal.classList.remove('fade');
+                modal.classList.add('show');
+            });
+        }
+
+        openModal(btnZooModal, zooModal);
+
+        openModal(btnPathfinderModal, pathfinderModal);
+    
+    });
 });
 
